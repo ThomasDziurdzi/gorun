c9df7c5a6b9ce4cd5ccae13904a0c208b6f132d3
Thomas Dziurdzi <63227823+ThomasDziurdzi@users.noreply.github.com>
2025-10-09 12:02:21 +0200

    feat/implement security controller for authentification

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/connexion', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('event_index');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/deconnexion', name: 'app_logout')]
+    public function logout(): void
+    {
+
+        throw new \LogicException(
+            'Cette méthode doit être interceptée par le firewall. ' .
+            'Vérifiez votre configuration security.yaml (section logout).'
+        );
+    }
+}
