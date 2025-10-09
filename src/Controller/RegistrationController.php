642c9ced1ccf319f10902ca85d3a321623e392bf
Thomas Dziurdzi <63227823+ThomasDziurdzi@users.noreply.github.com>
2025-10-09 11:43:46 +0200

    feat/implement user registration controller

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/inscription', name: 'app_register')]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $em
+    ): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('event_index');
+        }
+
+        $user = new User();
+
+        $form = $this->createForm(RegistrationFormType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plaintextPassword = $form->get('plainPassword')->getData();
+
+            $hashedPassword = $passwordHasher->hashPassword($user, $plaintextPassword);
+
+            $user->setPassword($hashedPassword);
+
+            $user->setRoles(['ROLE_USER']);
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre compte a été créé avec succès!');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm'  => $form,
+        ]);
+    }
+}
