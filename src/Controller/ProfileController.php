a9971e76858a7e8eaef7b6d03e492e10be34a310
Thomas Dziurdzi <thomasdziurdzi@gmail.com>
2025-11-20 11:43:45 +0100

    feat: implement profile edit functionnality in ProfileController

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,21 +2,48 @@
 
 namespace App\Controller;
 
+use App\Form\UserProfileType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/profil')]
+#[IsGranted('ROLE_USER')]
 class ProfileController extends AbstractController
 {
-    #[Route('/profil', name: 'app_profile')]
-    public function index(): Response
+    #[Route('', name: 'app_profile')]
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('profile/index.html.twig');
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Votre profil a été mis à jour avec succès.');
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('profile/index.html.twig', [
+            'form' => $form,
+            'user' => $user,
+        ]);
     }
 
-    #[Route('/profil/mes-evenements', name: 'app_profile_events')]
+    #[Route('/mes-evenements', name: 'app_profile_events')]
     public function myEvents(): Response
     {
-        return $this->render('profile/my_events.html.twig');
+        $user = $this->getUser();
+
+        return $this->render('profile/my_events.html.twig', [
+            'user' => $user,
+        ]);
     }
 }
